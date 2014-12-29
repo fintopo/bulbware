c30ec43ab5428da01ea9e032abde820ba95cfd1c
fintopo <rimura@fintopo.jp>
2014-12-30 08:28:23 +0900

    UserInfoが重複登録される不具合の修正

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,7 @@ from google.appengine.ext.webapp import template
 
 class Dashboard(webapp2.RequestHandler):
   def get(self):
+    user_model.get_login_userinfo() # userinfoが無い場合に生成するため
     path = os.path.join(os.path.dirname(__file__), 'dashboard.html')
     self.response.out.write(template.render(path, {
           'app': 'todo'
